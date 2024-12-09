32dc6015153225289538525ea13cf9da332a9aa0
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-09 18:58:45 +0900

    基本的な使い方

--- issue_02/script.js
+++ issue_02/script.js
@@ -17,7 +17,7 @@
         const mediaRecorder = new MediaRecorder(stream);
         // たくさんメソッドがある。レコードをコントロールするための
 
-        // なぜかonClickがきかなかったよ....ほえぇえ？
+        // なぜかonClickがきかなかったよ....ほえぇえ？ -> onclick() となっていたのが間違い
         record.onclick = () => {
           mediaRecorder.start();
           console.log(mediaRecorder.state);
@@ -26,13 +26,50 @@
           record.style.color = 'black';
         };
 
-        // record.addEventListener('click', () => {
-        //   mediaRecorder.start();
-        //   console.log(mediaRecorder.state);
-        //   console.log('recorder started');
-        //   record.style.background = 'red';
-        //   record.style.color = 'black';
-        // });
+        let chunks = [];
+        mediaRecorder.ondataavailable = (e) => {
+          chunks.push(e.data);
+        };
+
+        stop.onclick = () => {
+          mediaRecorder.stop();
+          console.log(mediaRecorder.state);
+          console.log('recorder stopped');
+          record.style.background = '';
+          record.style.color = '';
+        };
+
+        // ストップおされたら、チュンクを完成系にする
+
+        mediaRecorder.onstop = (e) => {
+          console.log('recorder stopped');
+
+          const clipName = prompt('Enter a name for your sound clip');
+
+          const clipContainer = document.createElement('article');
+          const clipLabel = document.createElement('p');
+          const audio = document.createElement('audio');
+          const deleteButton = document.createElement('button');
+
+          audio.setAttribute('controls', '');
+          deleteButton.textContent = 'Delete';
+          clipLabel.textContent = clipName;
+
+          clipContainer.appendChild(audio);
+          clipContainer.appendChild(clipLabel);
+          clipContainer.appendChild(deleteButton);
+          soundClips.appendChild(clipContainer);
+
+          const blob = new Blob(chunks, { type: 'audio/webm; codecs=opus' });
+          chunks = [];
+          const audioURL = window.URL.createObjectURL(blob);
+          audio.src = audioURL;
+
+          deleteButton.onclick = (e) => {
+            let evtTgt = e.target;
+            evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+          };
+        };
       })
       .catch((err) => {
         console.error('The following getUserMedia error occured:', err);
