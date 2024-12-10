d518c75ff7db856376ef675974843bb7859d2331
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-10 11:27:37 +0900

    単純な再生はこれでOK

---
+++ issue_04/script.js
@@ -0,0 +1,27 @@
+'use strict';
+{
+  const audioContext = new AudioContext();
+
+  const audioElement = document.querySelector('audio');
+  const track = audioContext.createMediaElementSource(audioElement);
+  track.connect(audioContext.destination);
+
+  const playButton = document.querySelector('button');
+  playButton.addEventListener(
+    'click',
+    () => {
+      if (audioContext.state === 'suspended') {
+        audioContext.resume();
+      }
+
+      if (playButton.dataset.playing === 'false') {
+        audioElement.play();
+        playButton.dataset.playing = 'true';
+      } else if (playButton.dataset.playing === 'true') {
+        audioElement.pause();
+        playButton.dataset.playing = 'false';
+      }
+    },
+    false
+  );
+}
