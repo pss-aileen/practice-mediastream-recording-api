655a9e92cd38a90017c5f74a1bdd45686ae91922
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-11 19:04:22 +0900

    初回のインプット確認画面ボタン

--- mini-app/script.js
+++ mini-app/script.js
@@ -3,6 +3,7 @@
   /* 
     [PROCESS]
     - フィードバック防止のために一旦音声は外に出さない
+    - マイクの許可を最初にもらう
     - ピッチを動かしたらピッチの値が変わるようにしておく（設定ボタンとかいらない）
     - 録音ボタンを押したら録音がはじまる
     - STOPしたら止まる
@@ -17,5 +18,35 @@
     - 「録音中」の雰囲気、テキストを表示する
     - 可能だったらファイルのサイズなんかも表示したい...が...
     - あと長さも！
+    - インプットの種類を将来的に選ぼう
   */
+
+  const recBtn = document.getElementById('rec');
+  const stopBtn = document.getElementById('stop');
+
+  const permissionBtn = document.getElementById('permission');
+
+  const mic = new Tone.UserMedia();
+  permissionBtn.addEventListener('click', () => {
+    mic
+      .open()
+      .then(() => {
+        console.log('mic open');
+      })
+      .catch((e) => console.error('error', e));
+  });
+
+  Tone.UserMedia.enumerateDevices().then((devices) => {
+    // print the device labels
+    console.log(devices.map((device) => device.label));
+  });
+
+  // mic
+  //   .open()
+  //   .then(() => {
+  //     console.log('mic open');
+  //   })
+  //   .catch((e) => {
+  //     console.error('mic not open:', e);
+  //   });
 }
